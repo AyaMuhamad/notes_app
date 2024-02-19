7d4c090a569bbdfaad3da64264c3e74f3b7b09da
Aya Mohamed <aya538925@gmail.com>
2024-02-19 22:23:49 +0200

    create note model

--- lib/views/Widgets/edit_note_view_body.dart
+++ lib/views/Widgets/edit_note_view_body.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/Widgets/custom_app_bar.dart';
 import 'package:notes_app/views/Widgets/custom_text_field.dart';
