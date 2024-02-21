9a9b963bf9f414d630b38a00414710cd2034fe95
Aya Mohamed <aya538925@gmail.com>
2024-02-21 23:53:27 +0200

    make hive know what adapter deal with

--- lib/cubits/add_note_cubit/note_cubit.dart
+++ lib/cubits/add_note_cubit/note_cubit.dart
@@ -1,8 +1,14 @@
 import 'package:meta/meta.dart';
 import 'package:bloc/bloc.dart';
+import 'package:notes_app/models/note_model.dart';
 
 import 'notes_state.dart';
 
 class NotesCubit extends Cubit<AddNotesState> {
   NotesCubit() : super(AddNotesinitial());
+
+
+  addNote(NoteModel note){
+    
+  }
 }
